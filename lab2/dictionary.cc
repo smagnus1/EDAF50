54996c651c8b6ca5a044c09badc7dd4440e9ff8b
smagnus1 <sunesson.magnus@gmail.com>
2019-02-14 14:13:57 +0100

    updated

--- lab2/dictionary.cc
+++ lab2/dictionary.cc
@@ -3,28 +3,44 @@
 #include <fstream>
 #include <iostream>
 #include <algorithm>
+#include <list>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
 #include "word.h"
 #include "dictionary.h"
 
 using std::string;
 using std::vector;
 
-Dictionary::Dictionary() {
-	public:
-		const contains(const string& word);
-		const get_suggestions(const sting& word);
-	private:
-		unordered_set<string> words;
-		std::ofstream unprocessedWords("words.txt");
 
+Dictionary::Dictionary() {
+	
+		const contains(const string& word);
+		const get_suggestions(const string& word);
+		
+		std::unordered_set<std::string> words;
+		std::ofstream unprocessedWords("words.txt");
+		
+		words << ofstream;
 }
 
 bool Dictionary::contains(const string& word) const {
-	if() return true;
+
+	std::unordered_set<std::string>::const_iterator find = words.find(word);
+
+	if (find == words.end()) {
+		return false;
+	} else {
+		return true;
+	}
+	
 	return false;
 }
 
-vector<string> Dictionary::get_suggestions(const string& word) const {
-	vector<string> suggestions;
-	return suggestions;
-}
+
+// vector<string> Dictionary::get_suggestions(const string& word) const {
+// 	vector<string> suggestions;
+// 	return const suggestions;
+// }
--- lab2/word.cc
+++ lab2/word.cc
@@ -5,12 +5,18 @@
 using std::vector;
 using std::string;
 
-Word::Word(const string& w, const vector<string>& t) {}
+/* Creates a word w with the sorted trigrams t */
+Word::Word(const string& w, const vector<string>& t) {
+	
+}
 
+/* Creates a word w with the sorted trigrams t */
 string Word::get_word() const {
 	return string();
 }
 
+/* Returns how many of the trigrams in t that are present*/
 unsigned int Word::get_matches(const vector<string>& t) const {
+	
 	return 0;
 }
